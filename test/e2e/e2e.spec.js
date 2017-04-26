ea8676804d6f6bd16f9d68396522b4e5cd20bdb0
Craig Cheney <ccheney@mit.edu>
2017-04-26 17:11:30 -0400

    Initial commit

---
+++ test/e2e/e2e.spec.js
@@ -0,0 +1,53 @@
+/* **********************************************************
+* File: e2e.spec.js
+*
+* Brief: Contains the End to End specifications for testing
+*   the mica Application.
+*
+* Author: Craig Cheney
+* Date: 2017.04.26
+*
+**********************************************************/
+import { Application } from 'spectron';
+import electronPath from 'electron';
+import path from 'path';
+/* Set default timeout */
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+/* Create an async delay */
+const delay = time => new Promise(resolve => setTimeout(resolve, time));
+describe('main window', function spec() {
+  /* Open a new electron window */
+  beforeAll(async () => {
+    this.app = new Application({
+      path: electronPath,
+      args: [path.join(__dirname, '..', '..', 'app')],
+    });
+    return this.app.start();
+  });
+  /* Close the window when done */
+  afterAll(() => {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop();
+    }
+  });
+
+  it('should open a window', async () => {
+    const { client, browserWindow } = this.app;
+
+    await client.waitUntilWindowLoaded();
+    await delay(500);
+    const title = await browserWindow.getTitle();
+    expect(title).toBe('micaReactElectron');
+  });
+  it('shouldn\'t have any logs in the console of the main window', async () =>{
+    const { client } = this.app;
+    const logs = await client.getRenderProcessLogs();
+    /* Print renderer process logs */
+    logs.forEach(log => {
+      console.log(log.message);
+      console.log(log.source);
+      console.log(log.level);
+    });
+    expect(logs).toHaveLength(0);
+  });
+});
