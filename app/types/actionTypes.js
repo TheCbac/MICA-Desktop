26927e842a21e62aa863893806b2b5f6a8d3fdf0
Craig Cheney <ccheney@mit.edu>
2017-06-07 15:31:15 -0400

    Add url to description

--- app/types/actionTypes.js
+++ app/types/actionTypes.js
@@ -3,7 +3,7 @@
 * File: types/actionTypes.js
 *
 * Brief: Types for the various actions
-*
+* https://blog.callstack.io/type-checking-react-and-redux-thunk-with-flow-part-2-206ce5f6e705
 * Author: Craig Cheney
 * Date: 2017.04.28
 *
@@ -15,11 +15,15 @@ type routeObject = {
   state: ?string,
   key: ?string
 };
-/* Defines the type for the action */
+
+
+/* Defines the type for the action - seems like this doesn't do anything */
 export type actionType = {
-  type: string,
-  payload: ?string | routeObject
+  type?: string,
+  payload?: ?string | routeObject
 };
 
+
+/* Different scan types available */
 export type scanTypes = 'usb' | 'ble';
 /* [] - END OF FILE */
