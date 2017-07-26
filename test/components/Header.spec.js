0c1cb6206a45906ee72d8785b06e44c64bb8bbbc
George Whitfield <gwhitfieldjr@gmail.com>
2017-07-26 15:19:09 -0400

    Wed July 26

--- test/components/Header.spec.js
+++ test/components/Header.spec.js
@@ -10,26 +10,50 @@
 *
 **********************************************************/
 // $FlowFixMe
-import renderer from 'react-test-renderer';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import { Router, createMemoryHistory } from 'react-router';
+import TestUtils from 'react-addons-test-utils';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Header from '../../app/components/Header';
 
-function setup(propsObj){
-  const component = mount(<Header 
-/>);
-return{
-  component
-};
+function setup() {
+  const component = shallow(<Header />);
+  return {
+    component
+  };
+}
 
+function createNodeMock(element) {
+  if (element.type === 'Header') {
+    return {};
+  }
+  return null;
 }
 /* Beginning of the test suite */
 
 describe('Header', () => {
-  it('Should render everything the same as the snapshot file', () => {
-    const header = renderer.create(<Header />).toJSON();
-    expect(header).toMatchSnapshot();
+  const { component } = setup();
+  it('Navbar should have props inverse and collaseOnSelect', () => {
+    const nav = component.find('Navbar');
+    expect(nav.at(0).prop('inverse')).not.toBeNull();
+    expect(nav.at(0).prop('collapseOnSelect')).not.toBeNull();
+  });
+  describe('IndexLinkContainers Test', () => {
+    it('Has correct values for props', () => {
+      const index = component.find('IndexLinkContainer');
+      expect(index.at(0).prop('activeClassName')).toEqual('active');
+      expect(index.at(1).prop('activeClassName')).toEqual('active');
+      expect(index.at(0).prop('to')).toEqual('/');
+      expect(index.at(1).prop('to')).toEqual('/collectData');
+    });
+  });
+  it('Renders correctly', () => {
+    const options = { createNodeMock };
+    const head = renderer.create(<Header />, options);
+    expect(head).toMatchSnapshot();
   });
 });
 
