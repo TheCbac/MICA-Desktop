261598dc1dbf1c1a15f0908e7c50900208a8d325
Craig Cheney <ccheney@mit.edu>
2017-11-07 15:18:00 -0500

    Response Packet parsing

--- app/utils/dataStreams/packets.js
+++ app/utils/dataStreams/packets.js
@@ -21,7 +21,10 @@ export const MICA_PACKET_ID_MODULE_POWER = 0x02;
 export const MICA_PACKET_ID_MODULE_SENSING = 0x03;
 export const MICA_PACKET_ID_MODULE_COMM = 0x04;
 export const MICA_PACKET_ID_MODULE_CONTROL = 0x05;
+export const MICA_PACKET_ID_MODULE_MAX = 0x05;
 
+export const MICA_PACKET_RESP_LEN_HEADER = 0x05;
+export const MICA_PACKET_RESP_LEN_FOOTER = 0x03;
 
 /* Construct a MICA packet from inputs */
 export function createMicaPacket(
@@ -41,10 +44,128 @@ export function createMicaPacket(
   return cmd;
 }
 
+export type packetDataT = number[] | Buffer;
+export type responsePacketT = {
+  status: number,
+  moduleId: number,
+  payload: packetDataT
+};
 /* Parse a MICA packet */
+export type micaResponseT = {
+  success: boolean,
+  error: string,
+  data?: responsePacketT
+};
+export type validationObjT = {
+  success: boolean,
+  error: string
+};
+/* Parse a response packet from a device */
+export function parseMicaResponse(packet: packetDataT): micaResponseT {
+  const responseObj = { success: false, error: '' };
+  const header = packet.slice(0, MICA_PACKET_RESP_LEN_HEADER);
+  const payload = packet.slice(MICA_PACKET_RESP_LEN_HEADER, -MICA_PACKET_RESP_LEN_FOOTER);
+  const footer = packet.slice(-MICA_PACKET_RESP_LEN_FOOTER);
+  /* Validate components */
+  const validHeader = validateResponseHeader(header);
+  if (!validHeader.success) {
+    responseObj.error = validHeader.error;
+    return responseObj;
+  }
+  const validFooter = validateResponseFooter(footer);
+  if (!validFooter.success) {
+    responseObj.error = validFooter.error;
+    return responseObj;
+  }
+  const validPayload = validateResponsePayload(header, payload);
+  if (!validPayload.success) {
+    responseObj.error = validPayload.error;
+    return responseObj;
+  }
+  const validChecksum = validateResponseChecksum(packet);
+  if (!validChecksum.success) {
+    responseObj.error = validChecksum.error;
+    return responseObj;
+  }
+  /* Packet is valid, unpack */
+  const moduleId = header[1];
+  const status = header[2];
+  const data = { moduleId, status, payload };
+  responseObj.data = data;
+  responseObj.success = true;
+  return responseObj;
+}
+/* Validate the header of the response packet */
+export function validateResponseHeader(header: packetDataT): validationObjT {
+  const result = { success: false, error: '' };
+  if (header.length !== MICA_PACKET_RESP_LEN_HEADER) {
+    result.error = 'Invalid header length';
+    return result;
+  }
+  if (header[0] !== MICA_PACKET_SYM_START) {
+    result.error = 'Invalid start symbol';
+    return result;
+  }
+  if (header[1] > MICA_PACKET_ID_MODULE_MAX) {
+    result.error = 'Invalid Module';
+    return result;
+  }
+  /* Return success */
+  result.success = true;
+  return result;
+}
+/* Validate the footer of the response packet */
+export function validateResponseFooter(footer: packetDataT): validationObjT {
+  const result = { success: false, error: '' };
+  if (footer.length !== MICA_PACKET_RESP_LEN_FOOTER) {
+    result.error = 'Invalid footer length';
+    return result;
+  }
+  if (footer[footer.length - 1] !== MICA_PACKET_SYM_END) {
+    result.error = 'Invalid end symbol';
+    return result;
+  }
+  /* Return success */
+  result.success = true;
+  return result;
+}
+
+/* Validate the checksum */
+export function validateResponseChecksum(packet: packetDataT): validationObjT {
+  const result = { success: false, error: '' };
+  const len = packet.length;
+  const msb = packet[len - 3];
+  const lsb = packet[len - 2];
+  const calc = calcChecksum16(packet.slice(0, -3));
+  if (calc.msb !== msb || calc.lsb !== lsb) {
+    result.error = 'Invalid checksum';
+    return result;
+  }
+  /* Return success */
+  result.success = true;
+  return result;
+}
+
+/* Validate the checksum */
+export function validateResponsePayload(
+  header: packetDataT,
+  payload: packetDataT
+): validationObjT {
+  const result = { success: false, error: '' };
+  const msb = header[3];
+  const lsb = header[4];
+  const reportedLength = (msb << SHIFT_BYTE_ONE) | lsb;
+  if (reportedLength !== payload.length) {
+    result.error = 'Invalid payload length';
+    return result;
+  }
+  /* Return success */
+  result.success = true;
+  return result;
+}
 
 /* Calculate the 16 bit checksum */
-export function calcChecksum16(dataArray: number[]): {
+export function calcChecksum16(dataArray: packetDataT): {
   msb: number,
   lsb: number
 } {
--- test/utils/dataStreams/packets.spec.js
+++ test/utils/dataStreams/packets.spec.js
@@ -10,7 +10,9 @@
 *
 ********************************************************* */
 import {
-  createMicaPacket, calcChecksum16,
+  createMicaPacket, parseMicaResponse, calcChecksum16,
+  validateResponseHeader, validateResponseFooter,
+  validateResponseChecksum, validateResponsePayload,
   MICA_PACKET_ID_MODULE_CONTROL,
   MICA_PACKET_SYM_START,
   MICA_PACKET_SYM_END
@@ -23,13 +25,301 @@ import {
 describe('Packets', () => {
   describe('createMicaPackets', () => {
     it('Should construct a packet with no payload', () => {
-      const packet = createMicaPacket(MICA_PACKET_ID_MODULE_CONTROL, MICA_PACKET_CTRL_CMD_BOOT);
+      const packet = createMicaPacket(
+        MICA_PACKET_ID_MODULE_CONTROL,
+        MICA_PACKET_CTRL_CMD_BOOT
+      );
       expect(packet[0]).toBe(MICA_PACKET_SYM_START);
       expect(packet[1]).toBe(MICA_PACKET_ID_MODULE_CONTROL);
       expect(packet[2]).toBe(MICA_PACKET_CTRL_CMD_BOOT);
       expect(packet[3]).toBe(0);
       expect(packet[4]).toBe(0);
       expect(packet[packet.length - 1]).toBe(MICA_PACKET_SYM_END);
+    });
+    it('Should create a packet with a payload', () => {
+      const payload = [0x01, 0x02, 0x03, 0xFF];
+      const packet = createMicaPacket(
+        MICA_PACKET_ID_MODULE_CONTROL,
+        MICA_PACKET_CTRL_CMD_BOOT,
+        payload
+      );
+      expect(packet[0]).toBe(MICA_PACKET_SYM_START);
+      expect(packet[1]).toBe(MICA_PACKET_ID_MODULE_CONTROL);
+      expect(packet[2]).toBe(MICA_PACKET_CTRL_CMD_BOOT);
+      expect(packet[3]).toBe(0);
+      expect(packet[4]).toBe(payload.length);
+      expect(packet[5]).toBe(payload[0]);
+      expect(packet[6]).toBe(payload[1]);
+      expect(packet[7]).toBe(payload[2]);
+      expect(packet[8]).toBe(payload[3]);
+      expect(packet[packet.length - 1]).toBe(MICA_PACKET_SYM_END);
+    });
+  });
+  describe('parseMicaResponse', () => {
+    it('should validate the header', () => {
+      const statusOk = 0x00;
+      /* Use end symbol */
+      let header = [MICA_PACKET_SYM_END, MICA_PACKET_ID_MODULE_CONTROL, statusOk, 0x00, 0x01];
+      let payload = [0xFD];
+      let { msb, lsb } = calcChecksum16([...header, ...payload]);
+      let footer = [msb, lsb, MICA_PACKET_SYM_END];
+      let packet = [...header, ...payload, ...footer];
+      let parsed = parseMicaResponse(packet);
+      expect(parsed.success).toBeFalsy();
+      expect(parsed.error).toBe('Invalid start symbol');
+      expect(parsed.data).toBeUndefined();
+
+      /* Invalid module */
+      header = [MICA_PACKET_SYM_START, MICA_PACKET_ID_MODULE_CONTROL + 10, statusOk, 0x00, 0x01];
+      payload = [0xFD];
+      ({ msb, lsb } = calcChecksum16([...header, ...payload]));
+      footer = [msb, lsb, MICA_PACKET_SYM_END];
+      packet = [...header, ...payload, ...footer];
+      parsed = parseMicaResponse(packet);
+      expect(parsed.success).toBeFalsy();
+      expect(parsed.error).toBe('Invalid Module');
+      expect(parsed.data).toBeUndefined();
+    });
+    it('should validate the footer', () => {
+      const statusOk = 0x00;
+      let header = [MICA_PACKET_SYM_START, MICA_PACKET_ID_MODULE_CONTROL, statusOk, 0x00, 0x01];
+      let payload = [0xFD];
+      let { msb, lsb } = calcChecksum16([...header, ...payload]);
+      /* swap endianess */
+      let footer = [lsb, msb, MICA_PACKET_SYM_END];
+      let packet = [...header, ...payload, ...footer];
+      let parsed = parseMicaResponse(packet);
+      expect(parsed.success).toBeFalsy();
+      expect(parsed.error).toBe('Invalid checksum');
+      expect(parsed.data).toBeUndefined();
+
+      header = [MICA_PACKET_SYM_START, MICA_PACKET_ID_MODULE_CONTROL, statusOk, 0x00, 0x01];
+      payload = [0xFD];
+      ({ msb, lsb } = calcChecksum16([...header, ...payload]));
+      /* Use start symbol */
+      footer = [msb, lsb, MICA_PACKET_SYM_START];
+      packet = [...header, ...payload, ...footer];
+      parsed = parseMicaResponse(packet);
+      expect(parsed.success).toBeFalsy();
+      expect(parsed.error).toBe('Invalid end symbol');
+      expect(parsed.data).toBeUndefined();
+    });
+    it('should validate the payload', () => {
+      const statusOk = 0x00;
+      /* Remove Packet id */
+      let header = [MICA_PACKET_SYM_START, /* MICA_PACKET_ID_MODULE_CONTROL, */ statusOk, 0x00, 0x01];
+      let payload = [0xFD];
+      let { msb, lsb } = calcChecksum16([...header, ...payload]);
+      let footer = [msb, lsb, MICA_PACKET_SYM_END];
+      let packet = [...header, ...payload, ...footer];
+      let parsed = parseMicaResponse(packet);
+      expect(parsed.success).toBeFalsy();
+      expect(parsed.error).toBe('Invalid payload length');
+      expect(parsed.data).toBeUndefined();
+
+      /* Change payload len endianess */
+      header = [MICA_PACKET_SYM_START, MICA_PACKET_ID_MODULE_CONTROL, statusOk, 0x01, 0x00];
+      payload = [0xFD];
+      ({ msb, lsb } = calcChecksum16([...header, ...payload]));
+      footer = [msb, lsb, MICA_PACKET_SYM_END];
+      packet = [...header, ...payload, ...footer];
+      parsed = parseMicaResponse(packet);
+      expect(parsed.success).toBeFalsy();
+      expect(parsed.error).toBe('Invalid payload length');
+      expect(parsed.data).toBeUndefined();
+    });
+    it('should validate the checksum', () => {
+      const statusOk = 0x00;
+      let header = [MICA_PACKET_SYM_START, MICA_PACKET_ID_MODULE_CONTROL, statusOk, 0x00, 0x01];
+      let payload = [0xFD];
+      let { msb, lsb } = calcChecksum16([...header, ...payload]);
+      /* Alter payload */
+      payload[0] = 0xDF;
+      let footer = [msb, lsb, MICA_PACKET_SYM_END];
+      let packet = [...header, ...payload, ...footer];
+      let parsed = parseMicaResponse(packet);
+      expect(parsed.success).toBeFalsy();
+      expect(parsed.error).toBe('Invalid checksum');
+      expect(parsed.data).toBeUndefined();
+
+      header = [MICA_PACKET_SYM_START, MICA_PACKET_ID_MODULE_CONTROL, statusOk, 0x00, 0x01];
+      payload = [0xFD];
+      ({ msb, lsb } = calcChecksum16([...header, ...payload]));
+      /* Change checksum endianess */
+      footer = [lsb, msb, MICA_PACKET_SYM_END];
+      packet = [...header, ...payload, ...footer];
+      parsed = parseMicaResponse(packet);
+      expect(parsed.success).toBeFalsy();
+      expect(parsed.error).toBe('Invalid checksum');
+      expect(parsed.data).toBeUndefined();
+    });
+    it('should accept valid packets', () => {
+      const statusOk = 0x00;
+      let header = [MICA_PACKET_SYM_START, MICA_PACKET_ID_MODULE_CONTROL, statusOk, 0x00, 0x01];
+      let payload = [0xFD];
+      let { msb, lsb } = calcChecksum16([...header, ...payload]);
+      let footer = [msb, lsb, MICA_PACKET_SYM_END];
+      let packet = [...header, ...payload, ...footer];
+      let parsed = parseMicaResponse(packet);
+      expect(parsed.success).toBeTruthy();
+      expect(parsed.error).toBe('');
+      expect(parsed.data).toEqual({
+        status: statusOk,
+        moduleId: MICA_PACKET_ID_MODULE_CONTROL,
+        payload
+      });
+
+      header = [MICA_PACKET_SYM_START, MICA_PACKET_ID_MODULE_CONTROL, statusOk, 0x01, 0x01];
+      payload = [];
+      for (let i = 0; i <= 256; i++) {
+        payload.push(i);
+      }
+      ({ msb, lsb } = calcChecksum16([...header, ...payload]));
+      footer = [msb, lsb, MICA_PACKET_SYM_END];
+      packet = [...header, ...payload, ...footer];
+      parsed = parseMicaResponse(packet);
+      expect(parsed.success).toBeTruthy();
+      expect(parsed.error).toBe('');
+      expect(parsed.data).toEqual({
+        status: statusOk,
+        moduleId: MICA_PACKET_ID_MODULE_CONTROL,
+        payload
+      });
+    });
+  });
+  describe('validateResponseHeader', () => {
+    it('should validate header for length', () => {
+      let header = [];
+      let result = validateResponseHeader(header);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid header length');
+
+      header = [0x00, 0x00, 0x00, 0x00];
+      result = validateResponseHeader(header);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid header length');
+    });
+    it('should validate header for Start symbol', () => {
+      let header = [0x00, 0x00, 0x00, 0x00, 0x00];
+      let result = validateResponseHeader(header);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid start symbol');
+
+      header = [0x02, 0x00, 0x00, 0x00, 0x00];
+      result = validateResponseHeader(header);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid start symbol');
+    });
+    it('should validate header for module ID', () => {
+      let header = [MICA_PACKET_SYM_START, 0x06, 0x00, 0x00, 0x00];
+      let result = validateResponseHeader(header);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid Module');
+
+      header = [MICA_PACKET_SYM_START, 0xFF, 0x00, 0x00, 0x00];
+      result = validateResponseHeader(header);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid Module');
+    });
+    it('should accept valid headers', () => {
+      let header = [MICA_PACKET_SYM_START, 0x00, 0x00, 0x00, 0x00];
+      let result = validateResponseHeader(header);
+      expect(result.success).toBeTruthy();
+      expect(result.error).toBe('');
+
+      header = [MICA_PACKET_SYM_START, 0x01, 0x01, 0x01, 0x01];
+      result = validateResponseHeader(header);
+      expect(result.success).toBeTruthy();
+      expect(result.error).toBe('');
+    });
+  });
+  describe('validateResponseFooter', () => {
+    it('should validate footer for length', () => {
+      let footer = [];
+      let result = validateResponseFooter(footer);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid footer length');
+
+      footer = [0x00, 0x00];
+      result = validateResponseFooter(footer);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid footer length');
+    });
+    it('should validate footer for end symbol', () => {
+      let footer = [0x00, 0x00, 0x00];
+      let result = validateResponseFooter(footer);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid end symbol');
+
+      footer = [0x00, 0x00, 0xA0];
+      result = validateResponseFooter(footer);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid end symbol');
+    });
+    it('should accept valid footers', () => {
+      let footer = [0x00, 0x00, MICA_PACKET_SYM_END];
+      let result = validateResponseFooter(footer);
+      expect(result.success).toBeTruthy();
+      expect(result.error).toBe('');
+
+      footer = [0xCC, 0xF0, MICA_PACKET_SYM_END];
+      result = validateResponseFooter(footer);
+      expect(result.success).toBeTruthy();
+      expect(result.error).toBe('');
+    });
+  });
+  describe('validateResponseChecksum', () => {
+    it('should validate based on reported checksum', () => {
+      let packet = [0x01, 0x00, 0x00, 0x00, 0x04, 0x0F, 0x00, 0xFF, 0x01, 0xFE, 0xEC, 0xAA];
+      let result = validateResponseChecksum(packet);
+      expect(result.success).toBeTruthy();
+      expect(result.error).toBe('');
+
+      packet = [0x01, 0x01, 0x00, 0x00, 0x04, 0x0F, 0x00, 0xFF, 0x01, 0xFE, 0xEC, 0xAA];
+      result = validateResponseChecksum(packet);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid checksum');
+
+      packet = [0x01, 0x00, 0x00, 0x00, 0x04, 0x0F, 0x00, 0xFF, 0x01, 0xEE, 0xEC, 0xAA];
+      result = validateResponseChecksum(packet);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid checksum');
+
+      packet = [0x01, 0x00, 0x00, 0x00, 0x04, 0x0F, 0x00, 0xFF, 0x01, 0xFE, 0xEE, 0xAA];
+      result = validateResponseChecksum(packet);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid checksum');
+    });
+  });
+  describe('validateResponsePayload', () => {
+    it('should validate length of the payload', () => {
+      let header = [MICA_PACKET_SYM_START, 0x00, 0x00, 0x00, 0x02];
+      let payload = [0xFD];
+      let result = validateResponsePayload(header, payload);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid payload length');
+
+      header = [MICA_PACKET_SYM_START, 0x00, 0x00, 0x01, 0x00];
+      payload = [0xFD];
+      result = validateResponsePayload(header, payload);
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBe('Invalid payload length');
+    });
+    it('should accept valid payloads', () => {
+      let header = [MICA_PACKET_SYM_START, 0x00, 0x00, 0x00, 0x01];
+      let payload = [0xFD];
+      let result = validateResponsePayload(header, payload);
+      expect(result.success).toBeTruthy();
+      expect(result.error).toBe('');
+
+      header = [MICA_PACKET_SYM_START, 0x00, 0x00, 0x01, 0x00];
+      payload = [];
+      for (let i = 0; i < 256; i++) {
+        payload.push(i);
+      }
+      result = validateResponsePayload(header, payload);
+      expect(result.success).toBeTruthy();
+      expect(result.error).toBe('');
     });
   });
   describe('calcChecksum16', () => {
